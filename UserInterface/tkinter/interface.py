744d313721422516fd1ffd9668428a52d74d2de7
angelachan64 <angelachan64@gmail.com>
2017-07-07 16:23:16 -0400

    Updates (see des)
    Optimized some of interpreter (more needs to be done for sure)
    Added the ability to draw circles
    Added another side panel that is accessible by clicking display code on the top right hand corner. It will display the code that is translated from the user inputs, along with line numbers next to them.
    Fixed the issue of the text box not scrolling to the bottom after having more than 35 or so lines.
    
    To be implemented next:
    The turtle cannot move outside the given canvas. Instead, it will trace the border.
    Code display was implemented to begin work on repeating commands, which will also be implemented next

--- UserInterface/tkinter/interface.py
+++ UserInterface/tkinter/interface.py
@@ -9,8 +9,9 @@ except ImportError:
 import turtle
 import interpreter
 
-yn_repeat=['n',0,0,0]
-# repeat, move, turn
+yn_repeat=['n',0,0,0,0,0]
+tf_display=False
+# repeat, move, turn (direction, degrees)
 class interface(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
@@ -20,15 +21,17 @@ class interface(Frame):
     def initUI(self):
         self.parent.title("PiE LOGO")
         self.pack(fill=BOTH, expand=1)
-        self.center()
+        self.center(900, 620)
 
         # Top heading bar
         frame1 = Frame(self)
         frame1.pack(fill=X)
         label_1 = Label(frame1, text="Welcome!", width = 9)
         label_1.pack(side=LEFT, padx = 5, pady = 5)
+        button_code = Button(frame1, text="Display Code")
         button_quit = Button(frame1, text="Quit", command=self.quit)
         button_quit.pack(side = RIGHT, padx = 5, pady = 5)
+        button_code.pack(side = RIGHT, padx = 5, pady = 5)
 
         # LOGO canvas
         frame2 = Frame(self)
@@ -47,10 +50,10 @@ class interface(Frame):
         frame4 = Frame(frame3)
         frame4.pack(side=TOP, fill=X)
         text_box = Text(frame4, height=35, width=33, state="disabled")
-        text_box.pack(side=LEFT)
         text_box.tag_configure('tag-right', justify='right')
+        text_box.pack(side=LEFT)
         scroll_bar = Scrollbar(frame4)
-        scroll_bar.pack(side=RIGHT, fill=Y)
+        scroll_bar.pack(side=LEFT, fill=Y)
         scroll_bar.config(command=text_box.yview)
         text_box.config(yscrollcommand=scroll_bar.set)
 
@@ -60,17 +63,26 @@ class interface(Frame):
         text_box.insert("end","Please input one command at\na time!\n")
         text_box.configure(state="disabled")
 
+        # Display translated code
+        code_box = Text(frame4, height=35, width=33, state="disabled")
+        scroll_bar2 = Scrollbar(frame4)
+        scroll_bar2.pack(side=RIGHT, fill=Y)
+        code_box.pack(side=RIGHT)
+        scroll_bar2.config(command=code_box.yview)
+        code_box.config(yscrollcommand=scroll_bar2.set)
+
         # Command entry
         frame5 = Frame(frame3)
         frame5.pack(side=BOTTOM, fill=X)
-        text_input = Entry(frame5, width=33)
-        text_input.bind("<Return>",(lambda event: self.command_entry(text_box,text_input,turt,yn_repeat,text_input.get())))
+        text_input = Entry(frame5, width=85)
+        text_input.bind("<Return>",(lambda event: self.command_entry(text_box,text_input,turt,yn_repeat,text_input.get(),code_box)))
         text_input.pack(side=LEFT)
         text_input.focus()
-        button_enter = Button(frame5, text="Submit", width=7, command=lambda:self.command_entry(text_box,text_input,turt,yn_repeat,text_input.get()))
+        button_enter = Button(frame5, text="Submit", width=7, command=lambda:self.command_entry(text_box,text_input,turt,yn_repeat,text_input.get(),code_box))
         button_enter.pack(side=RIGHT)
+        button_code.configure(command=lambda:self.display_code(text_input))
 
-    def command_entry(self,text_box,text_input,turt,yn_repeat1,command):
+    def command_entry(self,text_box,text_input,turt,yn_repeat1,command,code_box):
         global yn_repeat
         #print(yn_repeat)
         if len(command.split()) != 0:
@@ -88,54 +100,80 @@ class interface(Frame):
                     if item != 0:
                         if yn_repeat1.index(item) == 1:
                             if item == 1:
-                                yn_repeat = interpreter.interpret(self,turt,text_box,"forward %d" % num)
+                                yn_repeat = interpreter.interpret(self,turt,text_box,"forward %d" % num,code_box)
                             elif item == 2:
-                                yn_repeat = interpreter.interpret(self,turt,text_box,"backward %d" % num)
+                                yn_repeat = interpreter.interpret(self,turt,text_box,"backward %d" % num,code_box)
                         elif yn_repeat1.index(item)==2:
                             if item==1:
-                                yn_repeat=interpreter.interpret(self,turt,text_box,"turn right %d" % num)
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"turn right %d" % num,code_box)
                             elif item==2:
-                                yn_repeat=interpreter.interpret(self,turt,text_box,"turn left %d" % num)
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"turn left %d" % num,code_box)
                         elif yn_repeat1.index(item)==3:
                             if "right" in command_array:
                                 if item != -1:
-                                    yn_repeat=interpreter.interpret(self,turt,text_box,"turn right %d" % item)
+                                    yn_repeat=interpreter.interpret(self,turt,text_box,"turn right %d" % item,code_box)
                                 else:
-                                    yn_repeat=interpreter.interpret(self,turt,text_box,"turn right")
+                                    yn_repeat=interpreter.interpret(self,turt,text_box,"turn right",code_box)
                             elif "left" in command_array:
                                 if item != -1:
-                                    yn_repeat=interpreter.interpret(self,turt,text_box,"turn left %d" % item)
+                                    yn_repeat=interpreter.interpret(self,turt,text_box,"turn left %d" % item,code_box)
                                 else:
-                                    yn_repeat=interpreter.interpret(self,turt,text_box,"turn left")
+                                    yn_repeat=interpreter.interpret(self,turt,text_box,"turn left",code_box)
                         elif yn_repeat1.index(item)==4:
                             if "right" in command_array or "east" in command_array:
-                                yn_repeat=interpreter.interpret(self,turt,text_box,"east")
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"east",code_box)
                             elif "left" in command_array or "west" in command_array:
-                                yn_repeat=interpreter.interpret(self,turt,text_box,"west")
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"west",code_box)
                             elif "up" in command_array or "north" in command_array:
-                                yn_repeat=interpreter.interpret(self,turt,text_box,"north")
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"north",code_box)
                             elif "down" in command_array or "south" in command_array:
-                                yn_repeat=interpreter.interpret(self,turt,text_box,"south")
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"south",code_box)
                             else:
-                                yn_repeat=interpreter.interpret(self,turt,text_box,"face")
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"face",code_box)
+                        elif yn_repeat1.index(item)==5:
+                            if item==-1:
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"circle radius %d" % num,code_box)
+                            elif item==-2:
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"circle diameter %d" % num,code_box)
+                            elif item==-3:
+                                yn_repeat=interpreter.interpret(self,turt,text_box,"circle %d" % num,code_box)
+                            else:
+                                if "radius" in command_array:
+                                    yn_repeat=interpreter.interpret(self,turt,text_box,"circle radius %d" % yn_repeat[5],code_box)
+                                elif "diameter" in command_array:
+                                    yn_repeat=interpreter.interpret(self,turt,text_box,"circle diameter %d" % yn_repeat[5],code_box)
+                                else:
+                                    yn_repeat=interpreter.interpret(self,turt,text_box,"circle %d" % yn_repeat[5],code_box)
                             
             else:
-                yn_repeat = interpreter.interpret(self,turt,text_box,command)
+                yn_repeat = interpreter.interpret(self,turt,text_box,command,code_box)
         text_input.delete(0,'end')
         text_input.focus()
-            
+        text_box.see(END)
+        code_box.see(END)
+                    
 
-    def center(self):
-        w = 900
-        h = 620
+    def center(self, w, h):
         sw = self.parent.winfo_screenwidth()
         sh = self.parent.winfo_screenheight()
         x = (sw - w)/2
         y = (sh - h)/2 - 25
         self.parent.geometry("%dx%d+%d+%d" % (w, h, x, y))
+        
+    def display_code(self,text_input):
+        global tf_display
+        if tf_display:
+            self.center(900, 620)
+            tf_display = False
+        else:
+            self.center(1200,620)
+            tf_display = True
+        text_input.focus()
+        
 
 def main():
     root = Tk()
+    root.resizable(0,0)
     app = interface(root)
     root.mainloop()
 
--- UserInterface/tkinter/interpreter.py
+++ UserInterface/tkinter/interpreter.py
@@ -1,106 +1,38 @@
+import math
 import turtle
 from interface import interface
 
-def interpret(program,turt,text_box,command):
+line_number=1
+
+def interpret(program,turt,text_box,command,code_box):
     command = command.lower()
     command_array = command.split()
 
+    backward_array = ["backwards", "back", "backward"]
+    forward_array  = ["forward", "go", "forwards", "move"]
+    rotate_array   = ["rotate", "turn", "degrees"]
+    face_array     = ["face", "up", "down", "left", "right", "east", "west", "north", "south"]
+    circle_array   = ["circle","radius","diameter"]
+
     # Moving backward
-    if "backward" in command_array or "backwards" in command_array or "back" in command_array:
-        steps = 0;
-        for item in command_array:
-            if item.isdigit():
-                steps = int(item)
-                break
-        if steps != 0:
-            turt.backward(steps)
-            text_box.configure(state="normal")
-            text_box.insert("end","Okay! Moving backward %d steps!\n" % steps)
-            text_box.configure(state="disabled")
-        else:
-            text_box.configure(state="normal")
-            text_box.insert("end","Please enter a valid number\nof steps to move backward.\n")
-            text_box.configure(state="disabled")
-            return ['y', 2, 0, 0, 0]
+    if any(i in backward_array for i in command_array):
+        return backward(command_array,text_box,code_box,turt)
     
     # Moving forward
-    elif "go" in command_array or "forward" in command_array or "forwards" in command_array or "move" in command_array:
-        steps = 0
-        for item in command_array:
-            if item.isdigit():
-                steps = int(item)
-                break
-        if steps != 0:
-            turt.fd(steps)
-            text_box.configure(state="normal")
-            text_box.insert("end","Okay! Moving forward %d steps!\n" % steps)
-            text_box.configure(state="disabled")
-        else:
-            text_box.configure(state="normal")
-            text_box.insert("end","Please enter a valid number\nof steps to move forward.\n")
-            text_box.configure(state="disabled")
-            return ['y', 1, 0, 0, 0]
+    elif any(i in forward_array for i in command_array):
+        return forward(command_array,text_box,code_box,turt)
 
     # Turn a certain number of degrees
-    elif "rotate" in command_array or "turn" in command_array or "degrees" in command_array:
-        degrees = -1
-        for item in command_array:
-            if item.isdigit():
-                degrees=int(item)
-                break
-        if "right" in command_array or "left" in command_array:
-            if degrees == -1:
-                text_box.configure(state="normal")
-                text_box.insert("end","How many degrees would you\nlike the turtle to turn?\n")
-                text_box.configure(state="disabled")
-                if "right" in command_array:
-                    return ['y', 0, 1, 0, 0]
-                else:
-                    return ['y', 0, 2, 0, 0]
-            else:
-                if "right" in command_array:
-                    text_box.configure(state="normal")
-                    text_box.insert("end","Okay! Turning right %d degrees!\n" % degrees)
-                    text_box.configure(state="disabled")
-                    turt.right(degrees)
-                else:
-                    text_box.configure(state="normal")
-                    text_box.insert("end","Okay! Turning left %d degrees!\n" % degrees)
-                    text_box.configure(state="disabled")
-                    turt.left(degrees)
-        else:
-            text_box.configure(state="normal")
-            text_box.insert("end","Would you like the turtle to turn\nto the right or left?\n")
-            text_box.configure(state="disabled")
-            return ['y', 0, 0, degrees, 0]
-
+    elif any(i in rotate_array for i in command_array):
+        return rotate(command_array,text_box,code_box,turt)
+        
     # Face a certain direction
-    elif any(i in ['face','left','right','up','down','north','south','east','west'] for i in command_array):
-        if "left" in command_array or "west" in command_array:
-            text_box.configure(state="normal")
-            text_box.insert("end","Okay! Turning to the west!\n")
-            text_box.configure(state="disabled")
-            turt.setheading(180)
-        elif "up" in command_array or "north" in command_array:
-            text_box.configure(state="normal")
-            text_box.insert("end","Okay! Turning to the north!\n")
-            text_box.configure(state="disabled")
-            turt.setheading(90)
-        elif "right" in command_array or "east" in command_array:
-            text_box.configure(state="normal")
-            text_box.insert("end","Okay! Turning to the east!\n")
-            text_box.configure(state="disabled")
-            turt.setheading(0)
-        elif "down" in command_array or "south" in command_array:
-            text_box.configure(state="normal")
-            text_box.insert("end","Okay! Turning to the south!\n")
-            text_box.configure(state="disabled")
-            turt.setheading(270)
-        else:
-            text_box.configure(state="normal")
-            text_box.insert("end","What direction would you like\nthe turtle to face?\n")
-            text_box.configure(state="disabled")
-            return ['y', 0, 0, 0, 1]
+    elif any(i in face_array for i in command_array):
+        return face(command_array,text_box,code_box,turt)
+
+    # Make a circle with a certain radius or diameter
+    elif any(i in circle_array for i in command_array):
+        return circle(command_array,text_box,code_box,turt)
 
     # Clear the screen
     elif "clear" in command_array:
@@ -112,6 +44,7 @@ def interpret(program,turt,text_box,command):
         turt.home()
         turt.pendown()
 
+    # Quit the program
     elif "quit" in command_array:
         program.quit()
         
@@ -120,4 +53,183 @@ def interpret(program,turt,text_box,command):
         text_box.insert("end","Sorry, I didn't understand that.\nCould you rephrase that?\n")
         text_box.configure(state="disabled")
 
-    return ['n', 0, 0, 0, 0]
+    return ['n', 0, 0, 0, 0,0]
+
+# Move backwards
+def backward(command_array,text_box,code_box,turt):
+    steps = 0;
+    global line_number;
+    for item in command_array:
+        if item.isdigit():
+            steps = int(item)
+            break
+    if steps > 0:
+        text_box.configure(state="normal")
+        text_box.insert("end","Okay! Moving backward %d steps!\n" % steps)
+        text_box.configure(state="disabled")
+        code_box.configure(state="normal")
+        code_box.insert("end","%d: back(%d)\n" % (line_number,steps))
+        code_box.configure(state="disabled")
+        line_number = line_number + 1
+        turt.backward(steps)
+    else:
+        text_box.configure(state="normal")
+        text_box.insert("end","Please enter a valid number\nof steps to move backward.\n")
+        text_box.configure(state="disabled")
+        return ['y', 2, 0, 0, 0, 0]
+    return ['n', 0, 0, 0, 0, 0]
+
+# Move forward
+def forward(command_array,text_box,code_box,turt):
+    steps = 0
+    global line_number
+    for item in command_array:
+        if item.isdigit():
+            steps = int(item)
+            break
+    if steps > 0:
+        text_box.configure(state="normal")
+        text_box.insert("end","Okay! Moving forward %d steps!\n" % steps)
+        text_box.configure(state="disabled")
+        code_box.configure(state="normal")
+        code_box.insert("end","%d: fd(%d)\n" % (line_number,steps))
+        code_box.configure(state="disabled")
+        line_number = line_number + 1
+        turt.fd(steps)
+    else:
+        text_box.configure(state="normal")
+        text_box.insert("end","Please enter a valid number\nof steps to move forward.\n")
+        text_box.configure(state="disabled")
+        return ['y', 1, 0, 0, 0, 0]
+    return ['n', 0, 0, 0, 0, 0]
+
+# Rotate
+def rotate(command_array,text_box,code_box,turt):
+    degrees = -1
+    global line_number
+    for item in command_array:
+        if item.isdigit():
+            degrees=int(item)
+            break
+    if "right" in command_array or "left" in command_array:
+        if degrees <= -1:
+            text_box.configure(state="normal")
+            text_box.insert("end","How many degrees would you\nlike the turtle to turn?\n")
+            text_box.configure(state="disabled")
+            if "right" in command_array:
+                return ['y', 0, 1, 0, 0, 0]
+            else:
+                return ['y', 0, 2, 0, 0, 0]
+        else:
+            if "right" in command_array:
+                text_box.configure(state="normal")
+                text_box.insert("end","Okay! Turning right %d degrees!\n" % degrees)
+                text_box.configure(state="disabled")
+                code_box.configure(state="normal")
+                code_box.insert("end","%d: right(%d)\n" % (line_number, degrees))
+                code_box.configure(state="disabled")
+                turt.right(degrees)
+            else:
+                text_box.configure(state="normal")
+                text_box.insert("end","Okay! Turning left %d degrees!\n" % degrees)
+                text_box.configure(state="disabled")
+                code_box.configure(state="normal")
+                code_box.insert("end","%d: left(%d)\n" % (line_number, degrees))
+                code_box.configure(state="disabled")
+                turt.left(degrees)
+            line_number = line_number + 1
+    else:
+        text_box.configure(state="normal")
+        text_box.insert("end","Would you like the turtle to turn\nto the right or left?\n")
+        text_box.configure(state="disabled")
+        return ['y', 0, 0, degrees, 0,0]
+    return ['n', 0, 0, 0, 0,0]
+    
+def face(command_array,text_box,code_box,turt):
+     global line_number
+     if "left" in command_array or "west" in command_array:
+         text_box.configure(state="normal")
+         text_box.insert("end","Okay! Turning to the west!\n")
+         text_box.configure(state="disabled")
+         code_box.configure(state="normal")
+         code_box.insert("end","%d: setheading(180)\n" % line_number)
+         code_box.configure(state="disabled")
+         turt.setheading(180)
+     elif "up" in command_array or "north" in command_array:
+         text_box.configure(state="normal")
+         text_box.insert("end","Okay! Turning to the north!\n")
+         text_box.configure(state="disabled")
+         code_box.configure(state="normal")
+         code_box.insert("end","%d: setheading(90)\n" % line_number)
+         code_box.configure(state="disabled")
+         turt.setheading(90)
+     elif "right" in command_array or "east" in command_array:
+         text_box.configure(state="normal")
+         text_box.insert("end","Okay! Turning to the east!\n")
+         text_box.configure(state="disabled")
+         code_box.configure(state="normal")
+         code_box.insert("end","%d: setheading(0)\n" % line_number)
+         code_box.configure(state="disabled")
+         turt.setheading(0)
+     elif "down" in command_array or "south" in command_array:
+         text_box.configure(state="normal")
+         text_box.insert("end","Okay! Turning to the south!\n")
+         text_box.configure(state="disabled")
+         code_box.configure(state="disabled")
+         code_box.insert("end","%d: setheading(270)\n" % line_number)
+         turt.setheading(270)
+     else:
+         text_box.configure(state="normal")
+         text_box.insert("end","What direction would you like\nthe turtle to face?\n")
+         text_box.configure(state="disabled")
+         return ['y', 0, 0, 0, 1,0]
+     line_number = line_number + 1
+     return ['n',0,0,0,0,0]
+
+def circle(command_array,text_box,code_box,turt):
+    global line_number
+    num = 0
+    for item in command_array:
+        if item.isdigit():
+            num = int(item)
+            break
+    if num <= 0:
+        if "radius" in command_array:
+            text_box.configure(state="normal")
+            text_box.insert("end","What would you like the radius\nto be?\n")
+            text_box.configure(state="disabled")
+            return ['y', 0, 0, 0, 0, -1,0]
+        if "diameter" in command_array:
+            text_box.configure(state="normal")
+            text_box.insert("end","What would you like the\ndiameter to be?\n")
+            text_box.configure(state="disabled")
+            return ['y', 0, 0, 0, 0, -2]
+        else:
+            text_box.configure(state="normal")
+            text_box.insert("end","How large would you like the\ncircle to be?\n")
+            text_box.configure(state="disabled")
+            return ['y', 0, 0, 0, 0, -3]
+    else:
+        if "radius" in command_array:
+            text_box.configure(state="normal")
+            text_box.insert("end","Okay! Making a circle of\nradius %d!\n" % num)
+            text_box.configure(state="disabled")
+            code_box.configure(state="normal")
+            code_box.insert("end","%d: circle(%d)\n" % (line_number,num))
+            code_box.configure(state="disabled")
+            turt.circle(num)
+        elif "diameter" in command_array:
+            text_box.configure(state="normal")
+            text_box.insert("end","Okay! Making a circle of\ndiameter %d!\n" % num)
+            text_box.configure(state="disabled")
+            code_box.configure(state="normal")
+            code_box.insert("end","%d: circle(%d)\n" % (line_number,num/2))
+            code_box.configure(state="disabled")
+            turt.circle(num/2)
+        else:
+            text_box.configure(state="normal")
+            text_box.insert("end","Is the number provided the\ncircle's radius or diameter?\n")
+            text_box.configure(state="disabled")
+            return ['y', 0, 0, 0, 0, num]
+        line_number=line_number+1
+    return ['n',0,0,0,0,0]
